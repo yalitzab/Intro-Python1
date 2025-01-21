20aa1e95769246a2ecaceb6e6b4642c408704279
yalitzab <yalitzabueno@gmail.com>
2025-01-20 16:25:44 -0800

    lecture 2 complete

---
+++ Lecture_2.py
@@ -0,0 +1,88 @@
+#Parenthesis, Exponents, Multiplication, Division, Addition, Subtraction
+#PEMDAS
+
+# ((7+9)/2)*3
+# print(75%7)
+#
+# num1 = 7
+# num2 = 6
+# sum = num1 + num2
+# print(sum)
+
+## Area of a rectangle
+# width = 5
+# height = 10
+# area = width * height
+# print (width, ("*"), height, ("="), width * height)
+# print ("The area of the rectangle with a width of", width, "and a height of", height, "is", area)
+#
+# ## Perimeter of Rectangle
+# perimeter = (height + width) * 2
+# print ("The perimeter of a rectangle is:", perimeter)
+
+### Sarah buys 3 apples at $1.50 each and 2 bananas at $0.75 each.
+### She also has to pay a flat bag fee of $1. Calculate her total cost.
+# apple_cost = 1.50
+# num_apples = 3
+# banana_cost = 0.75
+# num_bananas = 2
+# bag_fee = 1
+#
+# total_apple_cost = apple_cost * num_apples
+# total_banana_cost = banana_cost * num_bananas
+# total_cost = total_apple_cost + total_banana_cost + bag_fee
+# # grand_total = (apple_cost * num_apples) + (banana_cost * num_bananas) + bag_fee
+# print ("Total cost is $", total_cost)
+
+########### Variables intiger, float, string
+
+# print (type(apple_cost))
+
+# var = 1
+# print(var,type(var))
+# print(str)
+# var = 2.1
+# print(var,type(var))
+# var = "5.1"
+# print(var,type(var))
+
+# input treats the variables as string
+# name = input("Please tell me your name:")
+# print("Hello",name,". nice to meet you")
+#
+# print(type(name))
+
+# first_name = input("Please tell me your first name: ")
+# last_name = input("Please tell me your last name: ")
+# movie = input("What is your favorite movie? ")
+#
+# print ("Hello", first_name, last_name,". Your favorite movie is:", movie)
+
+
+
+
+# int() #converts the inside to a intiger
+# float() # converts the inside to a float (decimal)
+# str() # Converts the inside to a string
+
+# width = int(input("What's the width of your rectangle? "))
+# height = int(input("What's the height of your rectangle? "))
+# area = width * height
+# perimeter = (height + width) * 2
+# # print (width, ("*"), height, ("="), width * height)
+# print ("The area of the rectangle with a width of", width, "and a height of", height, "is", area)
+# print ("The perimeter of a rectangle is:", perimeter)
+
+##### Ask the user what's their name and age. Then print back say "Hello <name> in 5 years you will be <age +5, years old
+name = input("What is your name: ")
+age = int(input("What's your age? "))
+years = 5
+future_age = age + years
+print ("Hello", name, "in", years, "years you will be", future_age, "years old.")
+
+
+
+
+
+
+
