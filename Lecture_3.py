f044b81b60e762f39e598a563650f9f950c569f1
Yalitza <yalitzabueno@gmail.com>
2025-02-03 13:57:50 -0800

    Update Lecture_3.py
    added notes if statement

--- Lecture_3.py
+++ Lecture_3.py
@@ -1,1 +1,21 @@
 #### Week 3 Lecture
+
+#This week, we are covering: 
+#Using if, else, and elif statements to make decisions. 
+#Comparison operators (>, <, ==, !=, >=, <=) to compare values. 
+#Formatting output with f-strings to include variables and perform calculations within strings. 
+
+### > < == != >= <=
+
+#if statement: 
+    #this line first 
+    #this line second
+    #lets and losts of lines
+    
+#if 8 > 10: 
+    #print ("statement was true")
+    #print("another line")
+    
+#print("hello world")
+
+###Ask name and if age is over 18 print you are an adult
