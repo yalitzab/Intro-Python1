e0dea87d6a93bd3dbae17bd4d50dd0454ef22d1e
Yalitza <yalitzabueno@gmail.com>
2025-02-03 16:57:25 -0800

    Update Lecture_3.py

--- Lecture_3.py
+++ Lecture_3.py
@@ -107,3 +107,9 @@ if score == 100:
 
 print("End program")
 
+####
+name = input("What is your name? ")
+age = int(input("What is you age? "))
+    
+print("Hello", name, "you are", age, "years old")
+print("print this out")
